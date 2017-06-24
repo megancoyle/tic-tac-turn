fe9816cb2f1ca5e56b87b956c04d90531c7e64a0
Megan Coyle <meg@mcoyle.com>
2017-06-24 16:18:09 -0400

    Add material UI

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import Home from '../containers/Home';
 import Profile from '../containers/Profile';
 import { Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
 
-class Header extends Component {
+injectTapEventPlugin()
+
+class Template extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <h1>TicTacTurn</h1>
-        </header>
-        <main>
-          <Route exact path="/"
-            component={Home}
-          />
-          <Route
-            path='/profile'
-            component={Profile}
-          />
-        </main>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>TicTacTurn</h1>
+            <RaisedButton
+            label={'Test Button'}
+            primary={true}
+            onTouchTap={()=>console.log('hello, it works')}
+            />
+          </header>
+          <main>
+            <Route exact path="/"
+              component={Home}
+            />
+            <Route
+              path='/profile'
+              component={Profile}
+            />
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default Header;
+export default Template;
