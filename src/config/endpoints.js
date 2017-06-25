0014e74123785730bb909bf944a085b88deb9875
Megan Coyle <meg@mcoyle.com>
2017-06-24 22:43:45 -0400

    Set up for Apollo instead of Relay

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,1 +1,1 @@
-export const relayApi = 'https://api.graph.cool/relay/v1/cj49vibmzi3yz0173q9m6071m'
+export const apiUrl = 'https://api.graph.cool/simple/v1/cj49vibmzi3yz0173q9m6071m'
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloClient } from 'react-apollo';
+import { apiUrl } from './config/endpoints';
+import auth from './utils/auth';
+
+const createHeaders = () => {
+  let idToken = auth.getToken()
+  if (idToken) {
+    Authorization: `Bearer ${idToken}`
+  } else {
+    return {}
+  }
+}
 
 ReactDOM.render(
     Routes,
